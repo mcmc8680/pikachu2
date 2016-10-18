af22ffa4dd0c0d91c10e4b7e284081ecb1e8a6a4
Marxism <moonmoonking@gmail.com>
2016-10-18 17:33:47 +0800

    3pika

--- pikachu.java
+++ pikachu.java
@@ -1,5 +1,7 @@
 public class pikachu {
 	public static void main(String[] args) {
 		System.out.println("Pika ! Pika !");
+		System.out.println("Pika ! Pika !");
+		System.out.println("Pika ! Pika !");
 	}
 }
